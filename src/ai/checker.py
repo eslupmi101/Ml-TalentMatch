e80fd6e02b3ffb695ae4e4e4422faebe742fef1e
koluzh <33715050+koluzh@users.noreply.github.com>
2024-02-29 22:42:23 +0900

    add evaluate function

--- src/ai/checker.py
+++
@@ -1,5 +0,0 @@
-import aspose.words as aw
-
-doc = aw.Document("/Users/koluj/Documents/GitHub/Ml-TalentMatch/src/ai/resumes/Alina   MANAFLI.pdf")
-# print(doc.to_string())
-doc.save("alina_manafli.md")
